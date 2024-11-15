cf576a1fd702076f1521a38e57048b70f26e8b8a
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-11-14 20:10:38 -0500

    Add Flyway

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,20 +1,32 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosRegistroMedico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
 
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
-                public void regitsraMedico(@RequestBody DatosRegistroMedico datosRegistroMedico) {
-            System.out.println(datosRegistroMedico);
+                public void regitsraMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico) {
+//            System.out.println(datosRegistroMedico);
+        medicoRepository.save(new Medico(datosRegistroMedico));
 
         }
+
+    @GetMapping
+    public List<Medico> listadoMedicos() {
+        return medicoRepository.findAll();
+    }
     }
 
--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,9 +1,31 @@
 package med.voll.api.medico;
 
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.direccion.DatosDireccion;
 
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion direccion) {
+public record DatosRegistroMedico(
+
+        @NotBlank
+        String nombre,
+
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String telefono,
+
+        @NotBlank
+        String documento,
+
+        @NotNull
+        Especialidad especialidad,
+
+@NotNull
+        @Valid
+        DatosDireccion direccion) {
 }
 
 
