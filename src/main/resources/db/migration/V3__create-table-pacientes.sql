63565aabe78cabb861134867ebdef5a66ca9af5e
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-11-16 22:29:09 -0500

    ADD ALTER TABLES

--- src/main/resources/db/migration/V3__create-table-pacientes.sql
+++ src/main/resources/db/migration/V3__create-table-pacientes.sql
@@ -4,13 +4,12 @@ create table pacientes(
     email varchar(100) not null unique,
     documento_identidad varchar(14) not null unique,
     telefono varchar(20) not null,
-    urbanización varchar(100) not null,
+    calle varchar(100) not null,
     distrito varchar(100) not null,
-    codigoPostal varchar(9) not null,
     complemento varchar(100),
     numero varchar(20),
-    provincia varchar(100) not null,
     ciudad varchar(100) not null,
 
     primary key(id)
-    )
+
+    );
