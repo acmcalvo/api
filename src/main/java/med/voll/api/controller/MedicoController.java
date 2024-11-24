9b5f883325a253cc3e32cad4b048d78dabb52fb0
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-11-24 18:04:08 -0500

    add validation

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,17 +16,10 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/medicos")
-
-@EnableSpringDataWebSupport(
-        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MedicoController {
-
-
 
     @Autowired
     private MedicoRepository medicoRepository;
-
-
 
     @PostMapping
     public ResponseEntity<DatosRespuestaMedico> registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico,
@@ -40,7 +32,6 @@ public class MedicoController {
                         medico.getDireccion().getComplemento()));
 
         URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
-
         return ResponseEntity.created(url).body(datosRespuestaMedico);
 
     }
--- src/main/java/med/voll/api/domain/paciente/DatosListadoPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DatosListadoPaciente.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.paciente;
 
 
+
 public record DatosListadoPaciente(Long id, String nombre, String email, String documento) {
 
     public DatosListadoPaciente(Paciente paciente) {
